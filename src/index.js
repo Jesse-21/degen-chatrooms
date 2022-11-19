0cb70adb99158e3c7741ce159c979b4dee3f0fab
Daft <daftdevs@gmail.com>
2022-11-19 21:52:05 +0700

    ref: prettier code formatting

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import actionCable from "actioncable";
 
 const CableApp = {};
-CableApp.cable = actionCable.createConsumer("wss://ancient-basin-80711.herokuapp.com/cable");
+CableApp.cable = actionCable.createConsumer(
+  "wss://ancient-basin-80711.herokuapp.com/cable"
+);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App cable={CableApp.cable} />
